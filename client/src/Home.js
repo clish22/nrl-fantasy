ce69f623e5b3032c790dc5c35d9ad56e6636ded3
Callum Lisha <clish22@gmail.com>
2023-03-13 22:51:55 +1000

    second commit

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,19 @@
-//react component for the home page
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 
-//react component for the home page
 function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('/api/nrl');
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+    console.log(data);
+  }, []);
+
   return (
     <div className="App">
       <h1>NRL Fantasy Draft</h1>
@@ -14,6 +23,8 @@ function Home() {
         your team. You can also view the teams of other users and compare them to your own team. You
         can also view the stats of all the players in the NRL.
       </p>
+      <h2>2023 NRL Ladder</h2>
+      {data ? <p>{data.message}</p> : <p>Loading...</p>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cheerio = require('cheerio');
+const axios = require('axios');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 //middleware
 app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -14,8 +16,37 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+// use cheerio to scrape data from website when /api/nrl is requested using async/await
+app.get('/api/nrl', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://www.nrl.com/ladder/?competition=111&round=2&season=2023'
+    );
+    const $ = cheerio.load(response.data);
+
+    // const data = [];
+    // find one piece of data as a test
+    // const test = $('.ladder__row').find('.ladder__item').text();
+
+    console.log($('[q-component=ladder-body-row]').find('.ladder__item'));
+    /*     $('.ladder__table tbody tr').each((i, element) => {
+      console.log($(element).find('.ladder__team').text());
+      data.push({
+        team: $(element).find('.ladder__team').text(),
+        played: $(element).find('.ladder__played').text(),
+        won: $(element).find('.ladder__won').text(),
+        drawn: $(element).find('.ladder__drawn').text(),
+        lost: $(element).find('.ladder__lost').text(),
+        for: $(element).find('.ladder__for').text(),
+        against: $(element).find('.ladder__against').text(),
+        points: $(element).find('.ladder__points').text(),
+      });
+    }); */
+    // res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
